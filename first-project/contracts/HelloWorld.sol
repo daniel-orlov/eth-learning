a00671fa1a696cdf75c9fd6fe7aade39801733ef
Daniel Orlov <daniel.orlov@protonmail.com>
2023-05-21 11:50:50 +0200

    feat: update HelloWorld contract

---
+++ first-project/contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+    string public message;
+
+    constructor() public {
+        message = "Hello World!";
+    }
+
+    function update(string memory _message) public {
+        message = _message;
+    }
+}
